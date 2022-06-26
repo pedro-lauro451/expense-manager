92a2ce65b432bbab12f371f1bb1e9ed7103a4ca1
Pedro <jinnpheonix@protonmail.com>
2022-06-26 20:14:31 -0300

    states module

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,10 @@
+import React from "react";
+import NewExpenseForm from "./NewExpenseForm";
+
+const NewExpense = () => {
+    return(
+        <NewExpenseForm/>
+    );
+};
+
+export default NewExpense;
